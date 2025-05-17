afff219958051bcd98c0746d0893c7234451f030
SagiYarden <yardenshagy@gmail.com>
2025-05-17 17:02:30 +0300

    affect all

--- apps/frontend/quotes-client/src/main.tsx
+++ apps/frontend/quotes-client/src/main.tsx
@@ -1,3 +1,4 @@
+// v1.0.0
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/app';
--- libs/quotes/interfaces/src/index.ts
+++ libs/quotes/interfaces/src/index.ts
@@ -1,3 +1,4 @@
+// v1.0.0
 export * from './lib/quotes-interfaces';
 export * from './lib/api-endpoints';
 export * from './lib/shared-interfaces';
