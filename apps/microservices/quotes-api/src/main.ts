cb94562440a8d2e5aa93203e0a3b732c3e9c5c4e
SagiYarden <yardenshagy@gmail.com>
2025-05-17 17:23:41 +0300

    Update main.ts

--- apps/microservices/quotes-api/src/main.ts
+++ apps/microservices/quotes-api/src/main.ts
@@ -7,10 +7,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:4200',
-      'https://quotes-of-the-day.onrender.com/',
-    ],
+    origin: ['http://localhost:4200', 'https://quotes-of-the-day.onrender.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
